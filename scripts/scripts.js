34262ba8a499cce8edafa6740b62d1c08fb0fbdb
Nicky <nickykwn@gmail.com>
2018-09-04 14:14:44 -0400

    added more content to each collapsible

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,3 +22,18 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
